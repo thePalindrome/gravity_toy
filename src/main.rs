d439d640c15b6fc7f91feb46341806cd40896ce2
Alex Ford <gordion42@gmail.com>
2020-02-07 00:40:29 -0700

    Add pausing, helptext, and draw trail toggling Also, performance bonus: static nodes no longer produce trails

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Ball {
 
 impl Ball {
     fn new(x: f32, y: f32) -> Ball {
-        Ball{ x: x, y: y, xv: 0.0, yv: 0.0, stationary: false, mass: 10.0, trail: VecDeque::with_capacity(500)}
+        Ball{ x: x, y: y, xv: 0.0, yv: 0.0, stationary: false, mass: 10.0, trail: VecDeque::with_capacity(50)}
     }
     fn distance_from(&self, other: &Ball) -> f32 {
         (self.x - other.x).hypot(self.y - other.y)
@@ -29,44 +29,51 @@ struct Game {
     xv: f32,
     yv: f32,
     mass: f32,
+    show_help: bool,
+    draw_trails: bool,
+    running: bool,
 }
 
 impl Game {
     fn new(_ctx: &mut Context) -> Game {
-        Game{ circle_vec: Vec::new(), stationary: false, xv: 0.0, yv: 0.0, mass: 10.0 }
+        Game{ circle_vec: Vec::new(), stationary: false, xv: 0.0, yv: 0.0, mass: 10.0, show_help: false, draw_trails: false, running: true }
     }
 }
 
 impl ggez::event::EventHandler for Game {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         while timer::check_update_time(ctx, 60) {
-            for i in 0..self.circle_vec.len() {
-                if self.circle_vec[i].stationary { continue; }
-                for j in 0..self.circle_vec.len() {
-                    if i == j {continue;}
-                    
-                    // TODO: Set some gravity
-                    // TODO: Incorporate Mass
-                    // θ = tan-1 ( y / x )
-                    // x = r × cos( θ )
-                    // y = r × sin( θ )
-                    // G: In SI units its value is approximately 6.674×10−11 m3⋅kg−1⋅s−2
-                    // F = G * ( (m1 * m2) / r^2)
+            if self.running {
+                for i in 0..self.circle_vec.len() {
+                    if self.circle_vec[i].stationary { continue; }
+                    for j in 0..self.circle_vec.len() {
+                        if i == j {continue;}
+                        
+                        // TODO: Set some gravity
+                        // TODO: Incorporate Mass
+                        // θ = tan-1 ( y / x )
+                        // x = r × cos( θ )
+                        // y = r × sin( θ )
+                        // G: In SI units its value is approximately 6.674×10−11 m3⋅kg−1⋅s−2
+                        // F = G * ( (m1 * m2) / r^2)
 
-                    let distance_mod = 0.006674 * ((self.circle_vec[j].mass * self.circle_vec[i].mass) / self.circle_vec[i].distance_from(&self.circle_vec[j])) / self.circle_vec[i].mass;
-                    let angle = (self.circle_vec[j].y - self.circle_vec[i].y).atan2(self.circle_vec[j].x - self.circle_vec[i].x);
+                        let distance_mod = 0.006674 * ((self.circle_vec[j].mass * self.circle_vec[i].mass) / self.circle_vec[i].distance_from(&self.circle_vec[j])) / self.circle_vec[i].mass;
+                        let angle = (self.circle_vec[j].y - self.circle_vec[i].y).atan2(self.circle_vec[j].x - self.circle_vec[i].x);
 
-                    self.circle_vec[i].xv += distance_mod * angle.cos();
-                    self.circle_vec[i].yv += distance_mod * angle.sin();
+                        self.circle_vec[i].xv += distance_mod * angle.cos();
+                        self.circle_vec[i].yv += distance_mod * angle.sin();
+                    }
                 }
-            }
-            for circle in self.circle_vec.iter_mut() {
-                if circle.trail.len() == circle.trail.capacity() {
-                    let _ = circle.trail.pop_back();
+                for circle in self.circle_vec.iter_mut() {
+                    if !circle.stationary {
+                        if circle.trail.len() == circle.trail.capacity() {
+                            let _ = circle.trail.pop_back();
+                        }
+                        circle.trail.push_front((circle.x,circle.y));
+                        circle.x += circle.xv;
+                        circle.y += circle.yv;
+                    }
                 }
-                circle.trail.push_front((circle.x,circle.y));
-                circle.x += circle.xv;
-                circle.y += circle.yv;
             }
         }
         Ok(())
@@ -74,34 +81,47 @@ impl ggez::event::EventHandler for Game {
     
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, graphics::BLACK);
+
+        draw_text(ctx, "Press F1 for help".to_string(), 128.0, 0.0);
+        if self.show_help {
+            draw_text(ctx,"Left click to place node\nLeft Arrow/Right Arrow to modify X velocity\nUp Arrow/Down Arrow to modify Y velocity\nLeft Shift/Right Shift to modify mass of node\nEnter/Return to reset velocity and mass to default values\nTab to pause simulation\nSpace to toggle \"Static\" mode\nBackspace to clear all nodes\nF2 to toggle trails (performance heavy)\nEscape to exit".to_string(), 128.0, 16.0);
+        }
+
         if self.xv != 0.0 {
-            graphics::queue_text(ctx, &graphics::Text::new(format!("xv: {}", self.xv)),mint::Point2{x:0.0,y:0.0},Some(graphics::DrawParam::default().color));
+            draw_text(ctx,format!("xv: {}", self.xv),0.0,0.0);
         }
         if self.yv != 0.0 {
-            graphics::queue_text(ctx, &graphics::Text::new(format!("yv: {}", self.yv)),mint::Point2{x:0.0,y:16.0},Some(graphics::DrawParam::default().color));
+            draw_text(ctx,format!("yv: {}", self.yv),0.0,16.0);
         }
         if self.stationary {
-            graphics::queue_text(ctx, &graphics::Text::new("Placing Static Node"),mint::Point2{x:0.0,y:32.0},Some(graphics::DrawParam::default().color));
+            draw_text(ctx,"Placing Static Node".to_string(),0.0,32.0);
         }
         if self.mass != 0.0 {
-            graphics::queue_text(ctx, &graphics::Text::new(format!("Mass: {}", self.mass)),mint::Point2{x:0.0,y:48.0},Some(graphics::DrawParam::default().color));
+            draw_text(ctx,format!("Mass: {}", self.mass),0.0,48.0);
         }
+        if !self.running {
+            draw_text(ctx,"Paused".to_string(),0.0,64.0);
+        }
+
+
         graphics::draw_queued_text(ctx, graphics::DrawParam::default(), None, graphics::FilterMode::Linear)?;
         for circle in self.circle_vec.iter() {
             let capacity = circle.trail.capacity() as f32;
             let mass = circle.mass.log10();
             let color = graphics::Color::new(((circle.xv.abs().hypot(circle.yv.abs())) / 10.0).min(1.0), 0.2, 0.2, 1.0);
-            for (id, trail) in circle.trail.iter().enumerate() {
-                let trail_modifier = 0.9 - ((id + 1) as f32 / capacity) as f32;
-                let trail_blit = graphics::Mesh::new_circle(
-                    ctx,
-                    graphics::DrawMode::fill(),
-                    mint::Point2{x:trail.0, y:trail.1},
-                    10.0 * mass * trail_modifier,
-                    0.1,
-                    color,
-                )?;
-                graphics::draw(ctx, &trail_blit, graphics::DrawParam::default())?;
+            if self.draw_trails {
+                for (id, trail) in circle.trail.iter().enumerate() {
+                    let trail_modifier = 0.9 - ((id + 1) as f32 / capacity) as f32;
+                    let trail_blit = graphics::Mesh::new_circle(
+                        ctx,
+                        graphics::DrawMode::fill(),
+                        mint::Point2{x:trail.0, y:trail.1},
+                        10.0 * mass * trail_modifier,
+                        0.1,
+                        color,
+                    )?;
+                    graphics::draw(ctx, &trail_blit, graphics::DrawParam::default())?;
+                }
             }
             let circle_blit = graphics::Mesh::new_circle(
                 ctx,
@@ -147,9 +167,16 @@ impl ggez::event::EventHandler for Game {
             KeyCode::RShift => {self.mass += 10.0;}
             KeyCode::LShift => {self.mass -= 10.0;}
             KeyCode::Return => {self.mass = 10.0; self.yv = 0.0; self.xv = 0.0;}
+            KeyCode::F1 => {self.show_help = !self.show_help;}
+            KeyCode::F2 => {self.draw_trails = !self.draw_trails;}
+            KeyCode::Tab => {self.running = !self.running;}
             _ => ()
         }
     }
+}
+
+fn draw_text(ctx: &mut Context, str: String, x: f32, y: f32) {
+    graphics::queue_text(ctx, &graphics::Text::new(str),mint::Point2{x:x,y:y},Some(graphics::DrawParam::default().color));
 }
 
 fn main() {
